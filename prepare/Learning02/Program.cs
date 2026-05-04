ece96b07c59bb944ce75a7b17a026cb4e7b6059c
BuffBard <tjalapenos@gmail.com>
2026-05-04 10:00:57 -0600

    prepare02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1=new Job();
+        job1._jobTitle="Software Engineer";
+        job1._company="Microsoft";
+        job1._firstYear=2019;
+        job1._lastYear=2022;
+
+        Job job2=new Job();
+        job2._jobTitle="Manager";
+        job2._company="Apple";
+        job2._firstYear=2022;
+        job2._lastYear=2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
